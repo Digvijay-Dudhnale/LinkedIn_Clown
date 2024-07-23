8358173acdc24e11f096cd56d7d5dbc58b5af1cf
Digvijay Dudhnale <dlearner421@gmail.com>
2024-07-23 18:18:44 +0530

    added responsiveness

--- script.js
+++ script.js
@@ -3,3 +3,17 @@ let profileMenu = document.querySelector("#profileMenu") ;
 function toggleMenu(){
     profileMenu.classList.toggle('open-menu') ;
 }
+
+// FOR RESPONSIVE DESIGN
+let sidebarActivity = document.querySelector(".sidebar-activity");
+let showMoreLink = document.querySelector("#show-more-link");
+
+function toggleActivity(){
+    sidebarActivity.classList.toggle('open-activity');
+    if(sidebarActivity.classList.contains("open-activity")){
+        showMoreLink.innerHTML = "Show Less <b>-</b>";
+    }
+    else{
+        showMoreLink.innerHTML = "Show More <b>+</b>";
+    }
+}
